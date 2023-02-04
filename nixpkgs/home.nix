1d46d3573ae527abb3e93a32f1a5e04eca8de4b6
Pat Long <pat@novasharper.net>
2023-02-04 13:27:33 -0500

    nixgl: move package detection to separate nix file

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -3,38 +3,7 @@
 with import <nixpkgs> {};
 let
   nixgl = import <nixgl> {};
-  # i915           = intel driver
-  # nvidia_modeset = nvidia driver
-  nixGLPackage =
-    let
-      _modulesFile =
-        runCommand "impure-loaded-modules-file" {
-          time = builtins.currentTime;
-          preferLocalBuild = true;
-          allowSubstitues = false;
-        } "cp /proc/modules $out 2> /dev/null || touch $out";
-      _driverMatch =
-        builtins.match
-        ".*video [0-9]+ [0-9]+ (([a-z0-9_]+,)+) .*"
-        (builtins.readFile _modulesFile);
-      videoDrivers =
-        let
-          data =
-            if _driverMatch != null
-            then builtins.split "," (builtins.head _driverMatch)
-            else [];
-        in
-          builtins.trace "[NixGL] Detected Drivers: ${builtins.toString data}" data;
-      intelPresent =
-        let
-          data = builtins.any (drv: drv == "i915") videoDrivers;
-          strv = if data then "true" else "false";
-        in
-          builtins.trace "[NixGL] Intel Present: ${strv}" data;
-    in
-      if intelPresent
-      then nixgl.nixGLIntel
-      else nixgl.auto.nixGLNvidia;
+  nixGLPackage = import ./nixgl-package.nix { inherit config pkgs lib; };
   nixGuiWrap = pkg: pkgs.runCommand "${pkg.name}-nixgui-wrapper" {
     preferLocalBuild = true;
   } ''
---
+++ nixpkgs/nixgl-package.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, lib, ... }:
+
+with import <nixpkgs> {};
+let
+  nixgl = import <nixgl> {};
+  _modulesFile =
+    runCommand "impure-loaded-modules-file" {
+      time = builtins.currentTime;
+      preferLocalBuild = true;
+      allowSubstitues = false;
+    } "cp /proc/modules $out 2> /dev/null || touch $out";
+  _driverMatch =
+    builtins.match
+    ".*video [0-9]+ [0-9]+ (([a-z0-9_]+,)+) .*"
+    (builtins.readFile _modulesFile);
+  videoDrivers =
+    let
+      data =
+        if _driverMatch != null
+        then builtins.split "," (builtins.head _driverMatch)
+        else [];
+    in
+      builtins.trace "[NixGL] Detected Drivers: ${builtins.toString data}" data;
+  # i915           = intel driver
+  # nvidia_modeset = nvidia driver
+  intelPresent =
+    let
+      data = builtins.any (drv: drv == "i915") videoDrivers;
+      strv = if data then "true" else "false";
+    in
+      builtins.trace "[NixGL] Intel Present: ${strv}" data;
+
+in
+  if intelPresent
+  then nixgl.nixGLIntel
+  else nixgl.auto.nixGLNvidia
