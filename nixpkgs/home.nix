8b720d9589bc606271db8f9ba3400a839cc9afbf
Pat Long <pat@novasharper.net>
2023-02-07 09:22:26 -0500

    nixgl: compose supported nixgl scripts
    Also, instead of setting specific gpu-context for mpv,
    just tell it to use opengl (vulkan seems to be broken
    with wayland).

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -3,34 +3,6 @@
 with import <nixpkgs> {};
 let
   nixgl = import ./nixgl-package.nix { inherit config pkgs lib; };
-  nixGuiWrap = pkg: pkgs.runCommandLocal
-    "${pkg.name}-nixgui-wrapper"
-    {
-      inherit (pkg) name meta;
-      inherit (pkg.version);
-    }
-    ''
-      mkdir $out
-      ln -s ${pkg}/* $out
-      rm $out/bin
-      mkdir $out/bin
-      # nixGL/Home Manager issue; https://github.com/guibou/nixGL/issues/44
-      # nixGL/Home Manager issue; https://github.com/guibou/nixGL/issues/114
-      # nixGL causes all software ran under it to gain nixGL status; https://github.com/guibou/nixGL/issues/116
-      # we wrap packages with nixGL; it customizes LD_LIBRARY_PATH and related
-      # envs so that nixpkgs find a compatible OpenGL driver
-      nixgl_bin="${lib.getExe nixgl.package}"
-      # Similar to OpenGL, the executables installed by nix cannot find the GTK modules
-      # required by the environment. The workaround is to unset the GTK_MODULES and
-      # GTK3_MODULES so that it does not reach for system GTK modules.
-      # We also need to modify the GTK_PATH to point to libcanberra-gtk3 installed via Nix
-      gtk_path="${lib.getLib pkgs.libcanberra-gtk3}/lib/gtk-3.0"
-      for bin in ${pkg}/bin/*; do
-        wrapped_bin=$out/bin/$(basename $bin)
-        echo "exec env GTK_MODULES= GTK3_MODULES= GTK_PATH=\"$gtk_path\" $nixgl_bin  $bin \"\$@\"" > $wrapped_bin
-        chmod +x $wrapped_bin
-      done
-    '';
   enable = x: x // { enable = true; };
 
 in {
@@ -53,9 +25,6 @@ in {
     };
     packages = with pkgs;
       [
-        # nixgl
-        nixgl.package
-
         # === languages ===
         # --- build ---
         meson
@@ -90,9 +59,9 @@ in {
         inkscape
         mypaint
         # --- AV ---
-        (nixGuiWrap celluloid)
-        (nixGuiWrap obs-studio)
-        (nixGuiWrap vlc)
+        (nixgl.wrap celluloid)
+        (nixgl.wrap obs-studio)
+        (nixgl.wrap vlc)
         # --- fonts ---
         office-code-pro
       ];
@@ -256,8 +225,9 @@ in {
             inherit lib stdenv fetchFromGitHub;
             inherit (pkgs) pkg-config dbus mpv-unwrapped;
           };
+
         in
-          nixGuiWrap (
+          nixgl.wrap (
             pkgs.wrapMpv pkgs.mpv-unwrapped {
               scripts = [ inhibit-gnome ];
             }
@@ -267,12 +237,10 @@ in {
         script-opts = with lib.strings; concatStringsSep "," [
           "ytdl_hook-ytdl_path=${lib.getExe pkgs.yt-dlp}"
         ];
-        gpu-context = "x11egl";
-      } // (
-        if nixgl.intelPresent
-        then { hwdec = "vaapi-copy"; }
-        else {}
-      );
+        # Vulkan API seems to be broken on Wayland
+        gpu-api = "opengl";
+        hwdec = "no";
+      };
     };
 
     tmux = enable {
--- nixpkgs/nixgl-package.nix
+++ nixpkgs/nixgl-package.nix
@@ -3,6 +3,55 @@
 with import <nixpkgs> {};
 let
   nixgl = import <nixgl> {};
+  guiWrapFactory = nixGLPkg: (
+    pkg: pkgs.runCommandLocal
+    "${pkg.name}-nixgui-wrapper"
+    {
+      inherit (pkg) name meta;
+      inherit (pkg.version);
+    }
+    ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      # nixGL/Home Manager issue; https://github.com/guibou/nixGL/issues/44
+      # nixGL/Home Manager issue; https://github.com/guibou/nixGL/issues/114
+      # nixGL causes all software ran under it to gain nixGL status; https://github.com/guibou/nixGL/issues/116
+      # we wrap packages with nixGL; it customizes LD_LIBRARY_PATH and related
+      # envs so that nixpkgs find a compatible OpenGL driver
+      nixgl_bin="${lib.getExe nixGLPkg}"
+      # Similar to OpenGL, the executables installed by nix cannot find the GTK modules
+      # required by the environment. The workaround is to unset the GTK_MODULES and
+      # GTK3_MODULES so that it does not reach for system GTK modules.
+      # We also need to modify the GTK_PATH to point to libcanberra-gtk3 installed via Nix
+      gtk_path="${lib.getLib pkgs.libcanberra-gtk3}/lib/gtk-3.0"
+      for bin in ${pkg}/bin/*; do
+        wrapped_bin=$out/bin/$(basename $bin)
+        echo "exec env GTK_MODULES= GTK3_MODULES= GTK_PATH=\"$gtk_path\" $nixgl_bin  $bin \"\$@\"" > $wrapped_bin
+        chmod +x $wrapped_bin
+      done
+    ''
+  );
+  # From https://github.com/guibou/nixGL/blob/main/nixGL.nix
+  writeExecutable = { name, text }:
+    writeTextFile {
+      inherit name text;
+
+      executable = true;
+      destination = "/bin/${name}";
+
+      checkPhase = ''
+        ${shellcheck}/bin/shellcheck "$out/bin/${name}"
+        # Check that all the files listed in the output binary exists
+        for i in $(${pcre}/bin/pcregrep  -o0 '/nix/store/.*?/[^ ":]+' $out/bin/${name})
+        do
+          ls $i > /dev/null || (echo "File $i, referenced in $out/bin/${name} does not exists."; exit -1)
+        done
+      '';
+    };
+
+  # ----
   _modulesFile =
     runCommand "impure-loaded-modules-file" {
       time = builtins.currentTime;
@@ -16,8 +65,8 @@ let
   videoModules =
     let
       data =
-        if _driverMatch != null
-        then builtins.split "," (builtins.head _driverMatch)
+        if _moduleMatch != null
+        then builtins.split "," (builtins.head _moduleMatch)
         else [];
     in
       builtins.trace "[NixGL] Detected Modules: ${builtins.toString data}" data;
@@ -29,11 +78,35 @@ let
       strv = if data then "true" else "false";
     in
       builtins.trace "[NixGL] Intel Present: ${strv}" data;
+  nvidiaPresent =
+    let
+      data = builtins.any (mod: mod == "nvidia_modeset") videoModules;
+      strv = if data then "true" else "false";
+    in
+      builtins.trace "[NixGL] NVIDIA Present: ${strv}" data;
+  packages =
+    lib.optional intelPresent nixgl.nixGLIntel
+    ++ lib.optional nvidiaPresent nixgl.auto.nixGLNvidia;
 
-in {
+
+in rec {
   inherit intelPresent;
+  inherit nvidiaPresent;
   package =
-    if intelPresent
-    then nixgl.nixGLCommon nixgl.nixGLIntel
-    else nixgl.nixGLCommon nixgl.auto.nixGLNvidia;
+    if (builtins.length packages) > 1
+    then writeExecutable {
+      name = "nixGLCompose";
+      text = ''
+        #!${runtimeShell}
+        exec ${
+          builtins.toString (
+            builtins.map
+              (pkg: lib.getExe (nixgl.nixGLCommon pkg))
+              packages
+          )
+        } "$@"
+      '';
+    }
+    else nixgl.auto.nixGLDefault;
+  wrap = guiWrapFactory package;
 }
