652c63fa19568b4a966d91634ac71ce2b1096a35
Pat Long <pat@novasharper.net>
2026-01-23 07:14:07 -0500

    darwin: Switch back to builtin application linking step

--- home-manager/darwin.nix
+++ home-manager/darwin.nix
@@ -1,29 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  disabledModules = ["targets/darwin/linkapps.nix"]; # to use custom aliasing instead
   home = {
-    activation = lib.mkIf pkgs.stdenv.isDarwin {
-      copyApplications =
-        let
-          apps = pkgs.buildEnv {
-            name = "home-manager-applications";
-            paths = config.home.packages;
-            pathsToLink = "/Applications";
-          };
-        in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          baseDir="$HOME/Applications/Home Manager Apps"
-          if [ -d "$baseDir" ]; then
-            $DRY_RUN_CMD rm -rf "$baseDir"
-          fi
-          $DRY_RUN_CMD mkdir -p "$baseDir"
-          for appFile in ${apps}/Applications/*; do
-            target="$baseDir/$(basename "$appFile")"
-            $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
-            $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
-          done
-        '';
-    };
     file = {
       ".local/bin/update-channel" = {
         executable = true;
