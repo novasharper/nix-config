55a6bae1d9c83d9953686e03f586dbbec71f6ddb
Pat Long <pat@novasharper.net>
2026-03-11 18:40:34 -0400

    switch to unstable channel

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,11 +2,11 @@
   description = "Darwin configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     fenix = {
@@ -28,37 +28,45 @@
     }:
     let
       inherit (self) outputs;
+      # Temp measure until cache for it is built
+      disableNodejsTesting =
+        final: prev:
+        {
+          nodejs_22 = prev.nodejs_22.overrideAttrs { doCheck = false; };
+          nodejs-slim_22 = prev.nodejs-slim_22.overrideAttrs { doCheck = false; };
+        };
 
     in
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-          allowUnfreePredicate = (_: true);
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            allowUnfreePredicate = (_: true);
+          };
+          overlays = [
+            inputs.fenix.overlays.default
+            inputs.nixgl.overlay
+            disableNodejsTesting
+          ];
         };
-        overlays = [
-          inputs.fenix.overlays.default
-          inputs.nixgl.overlay
-        ];
-      };
-    in
-    {
-      legacyPackages = {
-        homeConfigurations = {
-          pllong = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-              ./home.nix
-            ];
-            extraSpecialArgs = {
-              inherit inputs outputs pkgs;
-              nixVersion = "25.11";
+      in
+      {
+        legacyPackages = {
+          homeConfigurations = {
+            pllong = home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+              modules = [
+                ./home.nix
+              ];
+              extraSpecialArgs = {
+                inherit inputs outputs pkgs;
+                nixVersion = "unstable";
+              };
             };
           };
         };
-      };
-    }
+      }
     );
 }
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,7 +2,6 @@
 , config
 , pkgs
 , lib
-, fetchFromGitHub
 , nixVersion ? "unstable"
 , ...
 }:
