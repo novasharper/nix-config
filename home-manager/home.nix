1db880549bebba411e1a2052bed49babb8ae7c23
Pat Long <pat@novasharper.net>
2026-03-17 09:16:16 -0400

    vscode: re-enable raspberry pi pico extensions

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -317,13 +317,13 @@ in
           tamasfe.even-better-toml
           # Raspberry Pi Pico
           # Disabling until supports system toolchain
-          # raspberry-pi.raspberry-pi-pico
-          # paulober.pico-w-go
-          # marus25.cortex-debug
-          # mcu-debug.debug-tracker-vscode
-          # mcu-debug.memory-view
-          # mcu-debug.rtos-views
-          # mcu-debug.peripheral-viewer
+          raspberry-pi.raspberry-pi-pico
+          paulober.pico-w-go
+          marus25.cortex-debug
+          mcu-debug.debug-tracker-vscode
+          mcu-debug.memory-view
+          mcu-debug.rtos-views
+          mcu-debug.peripheral-viewer
         ];
         enableExtensionUpdateCheck = false;
         enableUpdateCheck = false;
