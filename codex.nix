2763ed0a5f9cd4f400b25de63890650e482eb323
Pat Long <pat@novasharper.net>
2026-04-25 14:36:02 -0400

    codex: update model

--- codex.nix
+++ codex.nix
@@ -18,7 +18,9 @@
         env_key = "LLM_AUTH_KEY";
       };
 
-      model = "qwen3.6-35b-a3b-ud-q8_k_kl";
+      # Model doesn't really matter. Only 'gpt-5.x' model names
+      # seem to do anything.
+      model = "qwen3-coder-next";
       model_context_window = 131072;
       model_provider = "ai-internal";
       model_reasoning_effort = "medium";
